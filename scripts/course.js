a0883945365feef46fcdeae2bfd6244bf75112e0
El-Luih <luisalejandro632@gmail.com>
2025-08-05 02:44:34 -0600

    minor changes

--- scripts/course.js
+++ scripts/course.js
@@ -133,20 +133,20 @@ wddFilter.addEventListener("click", function () {
 /*MODAL*/
 
 function displayCourseDetails(course) {
-  courseDetails.innerHTML = '';
-  courseDetails.innerHTML = `
-    <h2>${course.subject} ${course.number}</h2>
-    <button id="closeModal">✖</button>
-    <h3>${course.title}</h3>
-    <p><strong>Credits</strong>: ${course.credits}</p>
-    <p><strong>Certificate</strong>: ${course.certificate}</p>
-    <p>${course.description}</p>
-    <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
-  `;
-  courseDetails.showModal();
-  
-  closeModal.addEventListener("click", () => {
-    courseDetails.close();
-  });
+    courseDetails.innerHTML = '';
+    courseDetails.innerHTML = `
+        <h2>${course.subject} ${course.number}</h2>
+        <button id="closeModal">✖</button>
+        <h3>${course.title}</h3>
+        <p><strong>Credits</strong>: ${course.credits}</p>
+        <p><strong>Certificate</strong>: ${course.certificate}</p>
+        <p>${course.description}</p>
+        <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
+    `;
+    courseDetails.showModal();
+    
+    closeModal.addEventListener("click", () => {
+        courseDetails.close();
+    });
 }
 
